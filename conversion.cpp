e181f61a55c1da5e1f20e5b2fa5a059b817fd926
cluster777 <42055002+cluster777@users.noreply.github.com>
2019-10-18 22:32:42 +0700

    yang hex udh ok

--- conversion.cpp
+++ conversion.cpp
@@ -25,15 +25,17 @@ string bin2hex(string input){
 			pegang=input[i]+pegang;
 			continue;
 		}
-		int hitung=pegang[0]-'0'+(pegang[1]-'0')*2+(pegang[1]-'0')*4+(pegang[1]-'0')*8
+		int hitung=pegang[0]-'0'+(pegang[1]-'0')*2+(pegang[2]-'0')*4+(pegang[3]-'0')*8
 		if(hitung==10)hasil+='A';
 		else if(hitung==11)hasil+='B';
 		else if(hitung==12)hasil+='C';
 		else if(hitung==13)hasil+='D';
 		else if(hitung==14)hasil+='E';
 		else if(hitung==15)hasil+='F';
-		else hasil+=(char)((int)hitung+'0');
+		else hasil+=(char)(hitung+'0');
+		pegang.clear();
 	}
+	return hasil;
 }
 string hex2bin(string input){
 	string hasil="";
@@ -46,7 +48,7 @@ string hex2bin(string input){
 		else if(input[i]=='E')temp=14;
 		else if(input[i]=='F')temp=15;
 		else temp=(int)input[i]-'0';		
-		while(temp>0){
+		while(int j=0;j<4;j++){
 			if(temp%2==1)hasil+='1';
 			else hasil+='0';
 			temp/=2;
