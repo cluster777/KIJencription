c226ba613cb6765d66412423243bf5e7de962252
cluster777 <42055002+cluster777@users.noreply.github.com>
2019-10-18 22:14:36 +0700

    baru jadi bin

---
+++ conversion.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string tex2bin(string input){
+	string hasil="";
+	for(int i=0;i<input.size();i++){
+		int temp=(int)input[i];
+		while(temp>0){
+			if(temp%2==1)hasil+='1';
+			else hasil+='0';
+			temp/=2;
+		}
+	}
+}
+
+string bin2tex(string input){
+	
+}
+string bin2hex(string input){
+	
+}
+string hex2bin(string input){
+	string hasil="";
+	for(int i=0;i<input.size();i++){
+		int temp
+		if(input[i]=='A')temp=10;
+		else if(input[i]=='B')temp=11;
+		else if(input[i]=='C')temp=12;
+		else if(input[i]=='D')temp=13;
+		else if(input[i]=='E')temp=14;
+		else if(input[i]=='F')temp=15;
+		else temp=(int)input[i]-'0';		
+		while(temp>0){
+			if(temp%2==1)hasil+='1';
+			else hasil+='0';
+			temp/=2;
+		}
+	}
+}
+
+int main(){
+	
+}
