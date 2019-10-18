cb880e4823d230cd606623c64b583a2bd6273c11
cluster777 <42055002+cluster777@users.noreply.github.com>
2019-10-18 22:50:18 +0700

    fix tex conv

--- conversion.cpp
+++ conversion.cpp
@@ -5,7 +5,7 @@ string tex2bin(string input){
 	string hasil="";
 	for(int i=0;i<input.size();i++){
 		int temp=(int)input[i];
-		while(temp>0){
+		for(int j=0;j<8;j++){
 			if(temp%2==1)hasil+='1';
 			else hasil+='0';
 			temp/=2;
@@ -20,10 +20,10 @@ string bin2tex(string input){
 	for(int i=input.size()-1;i>=0;i--){
 		if(pegang.size()<8){
 			pegang=input[i]+pegang;
-			continue;
+			if(pegang.size()<8)continue;
 		}
 		int hitung=pegang[0]-'0'+(pegang[1]-'0')*2+(pegang[2]-'0')*4+(pegang[3]-'0')*8+(pegang[4]-'0')*16+(pegang[5]-'0')*32+(pegang[6]-'0')*64+(pegang[7]-'0')*128;
-		hasil+=(char)hitung;
+		hasil=(char)hitung+hasil;
 		pegang.clear();
 	}
 	return hasil;
